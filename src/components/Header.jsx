d68df73ec4cf2f80d02e7a9f4284090257516379
sumeetbandary <sumeetbandari@gmail.com>
2021-12-27 19:43:00 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+	return (
+		<header>
+			<h1>
+				<HighlightIcon />
+				Note Store
+			</h1>
+		</header>
+	);
+}
+
+export default Header;
